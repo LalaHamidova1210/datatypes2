72d42362e56b256b12515bcb0ca0f35428c11039
LalaHamidova1210 <lalahamidova1210@gmail.com>
2024-10-26 16:39:26 +0400

    first commit

---
+++ src/main/java/az/turingacademy/Main.java
@@ -0,0 +1,21 @@
+package az.turingacademy;
+
+public class Main {
+    public static void main(String[] args) {
+        byte a= 1;
+        short b=2;
+        int c=3;
+        long d=4L;
+        float e=5.456789f;
+        double f= 6;
+        char g= 'L' ;
+        boolean h=true;
+
+
+        System.out.print (a);
+        System.out.println(a+"\n"+b);
+        System.out.printf("name %s, age %d \n", "name", "age");
+        System.out.printf("name, age %.2f\n:", 50.0 );
+
+    }
+}
